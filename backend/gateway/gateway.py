7d0be6fed7767135b7586329ac9a4bb742db8d88
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2024-03-01 21:14:00 +0100

    fix: typo

--- backend/gateway/gateway.py
+++ backend/gateway/gateway.py
@@ -20,7 +20,7 @@ from uuid import uuid4
 
 # Load configuration from JSON file
 MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
-MQTT_PORT = int(os.environ.get("MQTT_HOST", 1883))
+MQTT_PORT = int(os.environ.get("MQTT_PORT", 1883))
 MQTT_USER = os.environ.get("MQTT_USER", None)
 MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD", None)
 MQTT_TLS = os.environ.get("MQTT_TLS", False)
