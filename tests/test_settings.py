e03e779f06ee47f0c3de126e459ad3b76169e5f2
BasarBayaz <twitchocean@gmail.com>
2024-09-10 13:26:55 +0000

    fix(merge conflict): trying to fix errors related to validation_alias

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -9,19 +9,17 @@ class TestSettings(BaseSettings):
 
     """
     GATEWAY_URL: AnyHttpUrl = Field(default="http://localhost:8000",
-                                    validation_alias=["GATEWAY_URL"])
+                                    alias="GATEWAY_URL")
     ORION_URL: AnyHttpUrl = Field(default="http://localhost:1026",
-                                  validation_alias=["ORION_URL"])
+                                  alias="ORION_URL")
     MQTT_HOST: str = Field(default="localhost",
-                           validation_alias=["MQTT_HOST"])
+                           alias="MQTT_HOST")
     MQTT_PORT: int = Field(default="1883",
-                           validation_alias=["MQTT_PORT"])
+                           alias="MQTT_PORT")
     FIWARE_SERVICE: str = Field(default="gateway_test",
-                                validation_alias=["FIWARE_SERVICE"])
+                                alias="FIWARE_SERVICE")
     FIWARE_SERVICEPATH: str = Field(default="/",
-                                    validation_alias=["FIWARE_PATH",
-                                         "FIWARE_SERVICEPATH",
-                                         "FIWARE_SERVICE_PATH"])
+                                    alias="FIWARE_SERVICEPATH")
     model_config = SettingsConfigDict(env_file=find_dotenv(".env"), env_file_encoding="utf-8", case_sensitive=False, use_enum_values=True, allow_reuse=True)
 
 
