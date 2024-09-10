4f39c9f28cbfb142ebd2d7ac74f0b8bf3aeaf2e5
BasarBayaz <twitchocean@gmail.com>
2024-09-10 13:06:41 +0000

    fix(merge conflict): trying to fix pydantic migration

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
+from pydantic import root_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
+
 
 class Settings(BaseSettings):
     ORION_URL: str = "http://orion:1026"
@@ -18,6 +20,14 @@ class Settings(BaseSettings):
     # Get log level from environment variable, default to INFO if not set
     LOG_LEVEL: str = 'INFO'  # 'critical', 'error', 'warning', 'info', 'debug'
     model_config = SettingsConfigDict(env_file=".env")
+
+    @root_validator(pre=True)
+    def validate_mqtt_credentials(cls, values):
+        if values.get("MQTT_TLS") and (
+                not values.get("MQTT_USER") or not values.get("MQTT_PASSWORD")):
+            raise ValueError(
+                "MQTT_USER and MQTT_PASSWORD must be set if MQTT_TLS is enabled.")
+        return values
 
 
 # Create an instance of the settings
