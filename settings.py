375876064e93072d3f9b52a9d57798f2bc687053
BasarBayaz <twitchocean@gmail.com>
2024-09-10 12:52:56 +0000

    fix(merge conflict): trying to fix pydantic migration

--- settings.py
+++ settings.py
@@ -1,14 +1,13 @@
-from pydantic import root_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
 
 class Settings(BaseSettings):
     ORION_URL: str = "http://orion:1026"
     MQTT_HOST: str = "test.mosquitto.org"
     MQTT_PORT: int = 1883
-    MQTT_USER: str = None
-    MQTT_PASSWORD: str = None
-    MQTT_TLS: bool = False
+    MQTT_USER: Optional[str] = None
+    MQTT_PASSWORD: Optional[str] = None
+    MQTT_TLS: Optional[bool] = False
     POSTGRES_HOST: str = "localhost"
     POSTGRES_USER: str = "admin"
     POSTGRES_PASSWORD: str = "postgres"
@@ -18,12 +17,6 @@ class Settings(BaseSettings):
     FIWARE_SERVICEPATH: str = "/"
     # Get log level from environment variable, default to INFO if not set
     LOG_LEVEL: str = 'INFO'  # 'critical', 'error', 'warning', 'info', 'debug'
-
-    @root_validator(pre=True)
-    def check_mqtt_user_and_password(cls, values):
-        if values.get("MQTT_TLS") and (not values.get("MQTT_USER") or not values.get("MQTT_PASSWORD")):
-            raise ValueError("MQTT_USER and MQTT_PASSWORD must be set if MQTT_TLS is enabled.")
-        return values
     model_config = SettingsConfigDict(env_file=".env")
 
 
