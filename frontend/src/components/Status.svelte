7dcc39aabeab804c690ad143e989949889126fac
JunsongDu <101181614+djs0109@users.noreply.github.com>
2024-09-23 16:43:11 +0200

    Update Status.svelte

--- frontend/src/components/Status.svelte
+++ frontend/src/components/Status.svelte
@@ -20,9 +20,9 @@
     <div class="status">
         <p>Services status:</p>
         {#if systemStatus}
-            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.orion ? 'green' : 'red'}"></span>Orion</p>
-            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.postgres ? 'green' : 'red'}"></span>Postgres</p>
-            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.redis ? 'green' : 'red'}"></span>Redis</p>
+            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.orion.status ? 'green' : 'red'}"></span>Orion</p>
+            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.postgres.status ? 'green' : 'red'}"></span>Postgres</p>
+            <p class="status-ok"><span class="circle" style="background-color:{systemStatus.redis.status ? 'green' : 'red'}"></span>Redis</p>
         {:else}
             <p class="status-error">Checking...</p>
         {/if}
